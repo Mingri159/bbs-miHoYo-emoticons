0b26b22ce8dc190c0a83e33ac84b5accc41f7085
shuai wang <1593683331@qq.com>
2024-08-09 17:33:46 +0800

    deploy

---
+++ sw.js
@@ -0,0 +1,45 @@
+const cacheName = "webstack-v1";
+const staticAssets = [
+  "./",
+  "./index.html",
+  "./public/app.css",
+  "./public/app.js",
+  "./manifest.json",
+];
+self.addEventListener("install", async (e) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener("activate", async (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", async (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
